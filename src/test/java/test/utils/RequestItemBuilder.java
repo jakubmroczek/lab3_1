04b9bb6d52f9a7ff229ba01d337d2ac9d18d1a6f
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-08 23:32:07 +0200

    builder implemented RequestItemBuilder

---
+++ src/test/java/test/utils/RequestItemBuilder.java
@@ -0,0 +1,31 @@
+package test.utils;
+
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class RequestItemBuilder {
+    
+    private ProductData productData = new ProductDataBuilder().build();
+    private int quantity = 1;
+    private Money totalCost = Money.ZERO;
+
+    public RequestItemBuilder withProductData(ProductData productData) {
+        this.productData = productData;
+        return this;
+    }
+
+    public RequestItemBuilder withQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public RequestItemBuilder withTotalCost(Money totalCost) {
+        this.totalCost = totalCost;
+        return this;
+    }
+
+    public RequestItem build() {
+        return new RequestItem(productData, quantity, totalCost);
+    }
+}
