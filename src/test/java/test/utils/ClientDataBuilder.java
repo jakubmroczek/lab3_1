32efb642ef70b614060503e7fdfd430b8c2cffc3
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-05-08 23:19:07 +0200

    builder implemented ClientDataBuilder

---
+++ src/test/java/test/utils/ClientDataBuilder.java
@@ -0,0 +1,24 @@
+package test.utils;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+public class ClientDataBuilder {
+
+    private Id id = Id.generate();
+    private String name = "John Does";
+
+    public ClientDataBuilder withId(Id id) {
+        this.id = id;
+        return this;
+    }
+
+    public ClientDataBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ClientData build() {
+        return new ClientData(id, name);
+    }
+}
